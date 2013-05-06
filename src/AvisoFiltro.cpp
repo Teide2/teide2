e798569da92fcb7c3c0027289f9c8b925f3ee138
salomeher25 <salomeher25@hotmail.com>
2013-05-06 08:24:15 +0000

    License type

--- src/AvisoFiltro.cpp
+++ src/AvisoFiltro.cpp
@@ -1,8 +1,24 @@
 /*******************************************************************************
-** AvisoFiltro.cpp: abre la ventana que indica si hay algún filtro que no permite
+** AvisoFiltro.cpp: abre la ventana que indica si hay algÃºn filtro que no permite
 **                  el NO_PROCEDE.
 **
-** author: M-S Hernández-García
+** author: M-S HernÃ¡ndez-GarcÃ­a
+**
+**  This file is part of TEIDE2.
+**
+**  TEIDE2 is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  Foobar is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+**
 *******************************************************************************/
 
 #include "AvisoFiltro.h"
