1711d0c4623b6000ff08f4df3e9fdf8c43501500
salomeher25@hotmail.com <salomeher25@hotmail.com@590bc0a3-9b52-101d-fc32-8273c4ad61bb>
2013-05-06 08:27:13 +0000

    License type

--- trunk/src/CambiarMicrodato.h
+++ trunk/src/CambiarMicrodato.h
@@ -1,8 +1,24 @@
 /*******************************************************************************
 ** CambiarMicrodato.h: permite modificar el microdato manualmente. Abre una
-**                       ventana para realizar la modificaciÛn.
+**                       ventana para realizar la modificaci√≥n.
 **
-** author: M-S Hern·ndez-GarcÌa
+** author: M-S Hern√°ndez-Garc√≠a
+**
+**  This file is part of TEIDE2.
+**
+**  TEIDE2 is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  Foobar is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+**
 *******************************************************************************/
 
 #ifndef CAMBIARMICRODATO_H
