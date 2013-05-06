30452347c3fc2cd222d1cd3751264b37dce19bd8
salomeher25@hotmail.com <salomeher25@hotmail.com@590bc0a3-9b52-101d-fc32-8273c4ad61bb>
2013-05-06 08:28:16 +0000

    License type

--- trunk/src/CheckPoblacion.h
+++ trunk/src/CheckPoblacion.h
@@ -1,7 +1,23 @@
 /*******************************************************************************
 ** CheckPoblacion.h:
 **
-** author: M-S Hernández-García
+** author: M-S HernÃ¡ndez-GarcÃ­a
+**
+**  This file is part of TEIDE2.
+**
+**  TEIDE2 is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  Foobar is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+**
 *******************************************************************************/
 
 #ifndef CHECKPOBLACION_H
@@ -80,7 +96,7 @@ public slots:
 
     //void ButChequearClick();
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// PULSAR EL BOTÓN DE GUARDAR INFORME.
+// PULSAR EL BOTÃ“N DE GUARDAR INFORME.
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     void ButGuardarInformeClick();
     void ButChequearClick();
