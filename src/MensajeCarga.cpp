84fe68e13f433a37cac02c6610f0b3ca93927d4f
salomeher25 <salomeher25@hotmail.com>
2013-05-06 09:06:49 +0000

    License type

--- src/MensajeCarga.cpp
+++ src/MensajeCarga.cpp
@@ -1,8 +1,24 @@
 /*******************************************************************************
-** MensajeCarga.cpp: muestra un mensaje de informaciÛn cuando ha terminado la
+** MensajeCarga.cpp: muestra un mensaje de informaci√≥n cuando ha terminado la
 **                   carga de los datos.
 **
-** author: M-S Hern·ndez-GarcÌa
+** author: M-S Hern√°ndez-Garc√≠a
+**
+**  This file is part of TEIDE2.
+**
+**  TEIDE2 is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  Foobar is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+**
 *******************************************************************************/
 
 #include "MensajeCarga.h"
