53a7a8be61308ee09f107e2e900e378c130bc4a1
salomeher25 <salomeher25@hotmail.com>
2013-05-06 08:41:28 +0000

    License type

--- src/RegAna.h
+++ src/RegAna.h
@@ -1,7 +1,25 @@
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Fichero de cabecera para el getter de coeficientes de ecuaciones de regresi�n.
+// Fichero de cabecera para el getter de coeficientes de ecuaciones de regresión.
 // 20 de Junio de 2005.
 // Sergio Delgado Quintero.
+//
+// Modificado M-S Hernández-García, Abril 2013
+//
+//  This file is part of TEIDE2.
+//
+//  TEIDE2 is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Foobar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+//
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #ifndef RegAnaH
 #define RegAnaH
