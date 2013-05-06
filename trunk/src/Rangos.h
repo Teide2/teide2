40ded5349ef33bfbd167289a8f597456ad74a1f0
salomeher25@hotmail.com <salomeher25@hotmail.com@590bc0a3-9b52-101d-fc32-8273c4ad61bb>
2013-05-06 09:13:22 +0000

    License type

--- trunk/src/Rangos.h
+++ trunk/src/Rangos.h
@@ -2,7 +2,23 @@
 ** Rangos.h: contiene todo lo necesario para trabajar con la solapa
 **             Eval. rangos.
 **
-** author: M-S Hernández-García
+** author: M-S HernÃ¡ndez-GarcÃ­a
+**
+**  This file is part of TEIDE2.
+**
+**  TEIDE2 is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  Foobar is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+**
 *******************************************************************************/
 
 #ifndef RANGOS_H
@@ -449,11 +465,11 @@ public slots:
 
     void ButCancelarCambiarMicrodatoClickR();
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// PINCHAR LA OPCION DE MENU DE AUMENTAR EL TAMAÑO DE LAS COLUMNAS.
+// PINCHAR LA OPCION DE MENU DE AUMENTAR EL TAMAÃ‘O DE LAS COLUMNAS.
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     void MitAumentarColumnasClick();
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// PINCHAR LA OPCION DE MENU DE DISMINUIR EL TAMAÑO DE LAS COLUMNAS.
+// PINCHAR LA OPCION DE MENU DE DISMINUIR EL TAMAÃ‘O DE LAS COLUMNAS.
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     void MitDisminuirColumnasClick();
 
