ea33f7b544da609276c33b18b4d41a052d0fb109
salomeher25@hotmail.com <salomeher25@hotmail.com@590bc0a3-9b52-101d-fc32-8273c4ad61bb>
2013-05-06 09:26:38 +0000

    License type

--- trunk/src/VariablesDeEdits.h
+++ trunk/src/VariablesDeEdits.h
@@ -2,6 +2,25 @@
 // Fichero de cabecera para el getter de Variables de Edits.
 // 14 de Agosto de 2003.
 // Sergio Delgado Quintero.
+//
+// Modificado M-S Hernández-García, Abril 2013
+//
+//  This file is part of TEIDE2.
+//
+//  TEIDE2 is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Foobar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+//
+
 //---------------------------------------------------------------------------
 #ifndef VariablesDeEditsH
 #define VariablesDeEditsH
